24e67727e992186ee3faf59b23ed445469c569ac
mudan <myudanov223@gmail.com>
2024-08-20 15:02:22 +0300

    added lombok to Person

--- src/main/java/ru/mud/Project2Boot/models/Person.java
+++ src/main/java/ru/mud/Project2Boot/models/Person.java
@@ -5,13 +5,15 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
-@Component
 @Entity
+@ToString
+@Data
+@NoArgsConstructor
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,50 +28,9 @@ public class Person {
     @OneToMany(mappedBy = "person",fetch = FetchType.LAZY)
     @OrderBy(value = "taken DESC")
     private final List<Book>bookList = new ArrayList<>();
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
-
-    public int getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(int birthday) {
-        this.birthday = birthday;
-    }
-
-    public Person() {
-    }
-
     public Person(int id, String info, int birthday) {
         this.id = id;
         this.info = info;
         this.birthday = birthday;
-    }
-
-    public List<Book> getBookList() {
-        return bookList;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", info='" + info + '\'' +
-                ", birthday=" + birthday +
-                '}';
     }
 }
