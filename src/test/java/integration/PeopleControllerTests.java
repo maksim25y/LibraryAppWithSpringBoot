cfe6d542709bd3926c6747e65bbeceea66f53d74
mudan <myudanov223@gmail.com>
2024-08-20 18:26:58 +0300

    added tests for get person by id and get edit in PeopleControlle

--- src/test/java/integration/PeopleControllerTests.java
+++ src/test/java/integration/PeopleControllerTests.java
@@ -12,10 +12,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.mud.Project2Boot.controllers.PeopleController;
+import ru.mud.Project2Boot.models.Book;
 import ru.mud.Project2Boot.models.Person;
 import ru.mud.Project2Boot.services.PeopleService;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,6 +51,40 @@ public class PeopleControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(view().name(VIEWS_PEOPLE+"new"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("person"));
+    }
+    @Test
+    public void shouldReturnPersonWithBooksWhenBooksCollectionIsNotEmpty() throws Exception {
+        Person person = getDefaultPerson();
+        List<Book>books = List.of(new Book());
+        when(peopleService.findById(person.getId())).thenReturn(Optional.of(person));
+        when(peopleService.getBooksByPersonId(person.getId())).thenReturn(books);
+        mockMvc.perform(MockMvcRequestBuilders.get(PEOPLE_URL+"/{id}", person.getId()).accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name(VIEWS_PEOPLE+"show"))
+                .andExpect(MockMvcResultMatchers.model().attribute("person",person))
+                .andExpect(MockMvcResultMatchers.model().attribute("books",books));
+    }
+    @Test
+    public void shouldReturnPersonWithNotBooksWhenBooksCollectionIsEmpty() throws Exception {
+        Person person = getDefaultPerson();
+        List<Book>books = List.of();
+        when(peopleService.findById(person.getId())).thenReturn(Optional.of(person));
+        when(peopleService.getBooksByPersonId(person.getId())).thenReturn(books);
+        mockMvc.perform(MockMvcRequestBuilders.get(PEOPLE_URL+"/{id}", person.getId()).accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name(VIEWS_PEOPLE+"show"))
+                .andExpect(MockMvcResultMatchers.model().attribute("person",person))
+                .andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("books"));
+    }
+    @Test
+    public void shouldHasAttributePersonWhenGetEditPerson() throws Exception {
+        Person person = getDefaultPerson();
+        when(peopleService.findById(person.getId())).thenReturn(Optional.of(person));
+        mockMvc.perform(MockMvcRequestBuilders.get(PEOPLE_URL+"/{id}/edit", person.getId())
+                        .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name(VIEWS_PEOPLE+"edit"))
+                .andExpect(MockMvcResultMatchers.model().attribute("person",person));
     }
     private Person getDefaultPerson(){
         return new Person(1,"Иванов Иван Иванович",1990);
